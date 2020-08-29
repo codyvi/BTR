44f27ffe89408ac984710fe8b0c2e3f8c86a0973
Bayli27 <58483682+Bayli27@users.noreply.github.com>
2020-08-29 16:57:53 -0500

    add .ignore and package

--- myfirstapp/src/main.js
+++ myfirstapp/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import '@aws-amplify/ui-vue';
 
 Vue.config.productionTip = false
 
